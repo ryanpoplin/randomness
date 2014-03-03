bd04abced1658dd17f100bf09f242ad8c9f9399e
Ryan Poplin <mobileupstate@gmail.com>
2014-03-03 03:08:20 -0500

    Adding...

--- app/scripts/app/app.js
+++ app/scripts/app/app.js
@@ -1,6 +1,6 @@
 (function() {
 	
-	var App, AppRegion, indexView, NameSpace;
+	var App, AppRegion, indexView, NameSpace, region;
 
     NameSpace = {
     	Models: {},
@@ -10,43 +10,54 @@
     };
 
     App = new Backbone.Marionette.Application();
+	
 	App.addRegions({
 		mainRegion: '#main'
 	});
+	
 	region = App.getRegion('mainRegion');
-	console.log(region);
 
     NameSpace.Views.IndexView = Backbone.Marionette.ItemView.extend({
         initialize: function() {
             console.log('IndexView Init');
         },
-        el: '#main',
-        template: _.template($('#index-template').html()),
+        el: '#main',					
+        template: Handlebars.compile($('#index-template').html()),
         render: function() {
+            region.reset();
             this.$el.html(this.template());
             return this;
         }
     });
 
     indexView = new NameSpace.Views.IndexView;
-                    
+
     App.Router = Backbone.Router.extend({
         initialize: function() {
             console.log('Router Init...');
         },
         routes: {
-            '': 'index'
+            '': 'index',
+            'menu': 'menu'
         },
         index: function() {
         	AppRegion = Marionette.Region.extend({
     			el: '#main',
     			currentView: indexView.render()
     		});
+    	},
+    	menu: function() {
+    		// ...
     	}
     });
 
     new App.Router;
 
     Backbone.history.start();
+	
+	console.log(region);
+
+	// ...
+	region.close(indexView);
 
 }());
